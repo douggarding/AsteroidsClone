69179d4cc5ffa5e8c77439891b89a0f7e4055b9f
Westley Kirkham <westleykirkham@westleys-mbp.uconnect.utah.edu>
2017-09-18 13:33:36 -0600

    make initial asteroids Xcode project

---
+++ xcode/asteroids.build/Debug/ALL_BUILD.build/Script-08449B87E31240C1968B420B.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/westleykirkham/DougAndWes/xcode -f /Users/westleykirkham/DougAndWes/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ xcode/asteroids.build/Debug/ZERO_CHECK.build/Script-3DA86921AB5247ECB4DFB9AE.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/westleykirkham/DougAndWes/xcode -f /Users/westleykirkham/DougAndWes/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
