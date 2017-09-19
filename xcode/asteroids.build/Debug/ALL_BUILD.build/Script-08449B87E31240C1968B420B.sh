d095e7b9ffca06518a24c5ee39f617197b87bc1f
Doug <doug.garding@outlook.com>
2017-09-19 09:39:16 -0600

    Got ride of xcode folder?

--- xcode/asteroids.build/Debug/ALL_BUILD.build/Script-08449B87E31240C1968B420B.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/westleykirkham/DougAndWes/xcode -f /Users/westleykirkham/DougAndWes/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- xcode/asteroids.build/Debug/ZERO_CHECK.build/Script-3DA86921AB5247ECB4DFB9AE.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/westleykirkham/DougAndWes/xcode -f /Users/westleykirkham/DougAndWes/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
