9f230a05b00658dc820d084f23d48b05b21e49d1
Doug <doug.garding@outlook.com>
2017-09-20 08:35:48 -0600

    A few minor tweaks

--- world.cpp
+++ world.cpp
@@ -24,7 +24,7 @@ world::world(int w, int h){
     height = h;
     playerShip = ship::ship(20, width/2 - 10, height/2 - 10);
     
-    // Create four level 3 asteroids for start of gae
+    // Create four level 3 asteroids for start of game
     for (int i = 0; i < 4; i++){
         int direction = rand() % 359 + 1;
         sf::Vector2f startPos = asteroidStartPosition();
@@ -34,30 +34,6 @@ world::world(int w, int h){
 }
 
 
-/**
- * HELPER METHOD - ASTEROID STARTING POSITION
- * Gets two random points within the game window. These two points will be at least
- * 400 pixels away from the position of the ship. This is to prevent an unfair scenario
- * where asteroids spawn so close to the ship that there is no fair opportunity to avoid them.
- */
-sf::Vector2f world::asteroidStartPosition(){
-    
-    // (x, y) values within world dimensions, but 400px away from ship:
-    int xPos = 0;
-    int yPos = 0;
-    int distance = 0;
-    do{
-        xPos = rand() % width;
-        yPos = rand() % height;
-        
-        // Distance formula to calculate distance between this coordinate and the ship coordinate
-        distance = sqrt(pow((xPos - playerShip.getPos().x), 2) + pow((yPos - playerShip.getPos().y), 2));
-        
-    } while (distance <= 400);
-    
-    sf::Vector2f startPosition(xPos, yPos);
-    return startPosition;
-}
 
 
 /**
@@ -87,28 +63,32 @@ void world::runWorld(){
         window.clear(sf::Color(44, 33, 68));
         
         
+        ///////////////////////
         // 1 - PROCESS USER INPUT
+        ///////////////////////
         
         playerShip.rotateRight();
         playerShip.rotateLeft();
         playerShip.thrusters(width, height);
         
-        
+        ///////////////////////
         // 2 - UPDATE GAME STATE
+        ///////////////////////
         
         // Update object locations
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < asteroids.size(); i++){
             asteroids[i].updatePosition();
         }
         // playerShip.updatePosition();
 
         
+        ///////////////////////
         // 3 - RENDER THE GAME
-        
+        ///////////////////////
         // Draw ship
         playerShip.drawShip(window);
         // Draw asteroids
-        for(int i = 0; i < 4; i++){
+        for(int i = 0; i < asteroids.size(); i++){
             asteroids[i].drawAsteroid(window);
         }
         
@@ -121,6 +101,31 @@ void world::runWorld(){
     
 }
 
+
+/**
+ * HELPER METHOD - ASTEROID STARTING POSITION
+ * Gets two random points within the game window. These two points will be at least
+ * 400 pixels away from the position of the ship. This is to prevent an unfair scenario
+ * where asteroids spawn so close to the ship that there is no fair opportunity to avoid them.
+ */
+sf::Vector2f world::asteroidStartPosition(){
+    
+    // (x, y) values within world dimensions, but 400px away from ship:
+    int xPos = 0;
+    int yPos = 0;
+    int distance = 0;
+    do{
+        xPos = rand() % width;
+        yPos = rand() % height;
+        
+        // Distance formula to calculate distance between this coordinate and the ship coordinate
+        distance = sqrt(pow((xPos - playerShip.getPos().x), 2) + pow((yPos - playerShip.getPos().y), 2));
+        
+    } while (distance <= 400);
+    
+    sf::Vector2f startPosition(xPos, yPos);
+    return startPosition;
+}
 
 /*
  Say you want your game to run at 60 FPS. That gives you about 16 milliseconds per frame. 
